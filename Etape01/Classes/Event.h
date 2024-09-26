39f95f45ce60c0e070098ea1ebdaf005f293e254
Grisonnet1495 <tomahsty@gmail.com>
2024-09-26 09:13:52 +0200

    Reorganisation des fichiers comme voulu par la professeur

---
+++ Etape01/Classes/Event.h
@@ -0,0 +1,25 @@
+#ifndef EVENT_H
+#define EVENT_H
+
+class Event
+{
+  private:
+    int code;
+    char *title;
+
+  public:
+    Event();
+    Event(int c, const char *t);
+    Event(const Event &E);
+    ~Event();
+
+    void setCode(int c);
+    void setTitle(const char *t);
+
+    int getCode()const;
+    const char* getTitle() const;
+
+    void display() const;
+};
+
+#endif
