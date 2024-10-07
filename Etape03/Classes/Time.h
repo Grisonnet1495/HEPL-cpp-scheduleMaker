ffba15045ed8c8572d02974c2cedf3bde7883fa6
Grisonnet1495 <tomahsty@gmail.com>
2024-10-07 20:23:13 +0200

    Correction du fichier Time.cpp pour l'etape 3

--- Etape03/Classes/Time.h
+++ Etape03/Classes/Time.h
@@ -12,7 +12,7 @@ namespace planning
 	{
 		friend Time operator+(int minutesNbr, Time T);
 		friend Time operator-(int minutesNbr, Time T);
-		friend ostream& operator<<(ostream& s,const Time& T);
+		friend ostream& operator<<(ostream& s, const Time& T);
 		friend istream& operator>>(istream& s, Time& T);
 
 		private:
