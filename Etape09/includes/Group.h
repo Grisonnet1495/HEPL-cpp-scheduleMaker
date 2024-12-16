d9c99ccab6ea6573b6428f34f85f9cfdad50b220
Grisonnet1495 <tomahsty@gmail.com>
2024-12-16 14:26:44 +0100

    Fin de l'etape 9

---
+++ Etape09/includes/Group.h
@@ -0,0 +1,34 @@
+#ifndef GROUP_H
+#define GROUP_H
+
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+#include "Schedulable.h"
+using namespace planning;
+
+class Group : public Schedulable
+{
+  friend ostream& operator<<(ostream& s, const Group& g);
+
+  private:
+    string name;
+
+  public:
+    Group();
+    Group(int id, const string n);
+    Group(const Group& g);
+    ~Group();
+
+    void setName(const string n);
+
+    const string& getName() const;
+
+    Group& operator=(const Group& g);
+    bool operator<(const Group& g) const;
+
+    string toString() const override;
+    string tuple() const override;
+};
+
+#endif
---
+++ Etape09/sources/Group.cpp
@@ -0,0 +1,89 @@
+#include "Group.h"
+
+using namespace std;
+
+// Constructeurs
+
+Group::Group():Schedulable()
+{
+    #ifdef DEBUG
+      cout << ">>> Appelle du constructeur par defaut de Group" << endl;
+    #endif
+
+    setName("---");
+}
+
+Group::Group(int id, const string n):Schedulable(id)
+{
+    #ifdef DEBUG
+      cout << ">>> Appelle du constructeur d'initialisation de Group" << endl;
+    #endif
+
+    setName(n);
+}
+
+Group::Group(const Group& g):Schedulable(g)
+{
+    #ifdef DEBUG
+      cout << ">>> Appelle du constructeur de copie de Group" << endl;
+    #endif
+
+    setName(g.name);
+}
+
+// Destructeur
+
+Group::~Group()
+{
+    #ifdef DEBUG
+      cout << ">>> Appelle du destructeur de Group" << endl;
+    #endif
+}
+
+// Setters
+
+void Group::setName(const string n)
+{
+    name = n;
+}
+
+// Getters
+
+const string& Group::getName() const
+{
+  return name;
+}
+
+// Méthodes d'instances
+
+string Group::toString() const
+{
+    return "[id=" + to_string(id) + "] " + name;
+}
+
+string Group::tuple() const
+{
+    return to_string(id) + ";" + name;
+}
+
+// Méthodes de surcharge d'opérateurs
+
+Group& Group::operator=(const Group& g)
+{
+    Schedulable::operator=(g);
+    name = g.name;
+
+    return (*this);
+}
+
+bool Group::operator<(const Group& g) const
+{
+    return name < g.name;
+}
+
+ostream& operator<<(ostream& s, const Group& g)
+{
+    s << g.toString();
+
+    return s;
+}
