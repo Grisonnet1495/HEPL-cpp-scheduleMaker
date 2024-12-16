21a542ab2a106ca1c799eb2c4089a9662d64aaa9
Grisonnet1495 <tomahsty@gmail.com>
2024-12-16 14:26:01 +0100

    Fin de l'etape 8b

---
+++ Etape08b/Classes/Exception/TimetableException.h
@@ -0,0 +1,29 @@
+#ifndef TIMETABLE_EXCEPTION_H
+#define TIMETABLE_EXCEPTION_H
+
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+#include "Exception.h"
+using namespace std;
+
+class TimetableException : public Exception
+{
+  private:
+    int code;
+
+  public:
+    static const int INVALID_INDEX = 1;
+    static const int INVALID_ID = 2;
+
+    TimetableException();
+    TimetableException(const string& m, int c);
+    TimetableException(const TimetableException& t);
+    ~TimetableException();
+
+    void setCode(int c);
+
+    int getCode() const;
+};
+
+#endif
---
+++ Etape08b/Classes/Structure/Classroom.h
@@ -0,0 +1,38 @@
+#ifndef CLASSROOM_H
+#define CLASSROOM_H
+
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+#include "Schedulable.h"
+using namespace planning;
+
+class Classroom : public Schedulable
+{
+  friend ostream& operator<<(ostream& s, const Classroom& c);
+  friend istream& operator>>(istream& s, Classroom& c);
+
+  private:
+    string name;
+    int seatingCapacity;
+
+  public:
+    Classroom();
+    Classroom(int id, const string& n, int s);
+    Classroom(const Classroom& c);
+    ~Classroom();
+
+    void setName(const string& n);
+    void setSeatingCapacity(int n);
+
+    const string& getName() const;
+    int getSeatingCapacity() const;
+
+    Classroom& operator=(const Classroom& c);
+    bool operator<(const Classroom& c) const;
+
+    string toString() const override;
+    string tuple() const override;
+};
+
+#endif
